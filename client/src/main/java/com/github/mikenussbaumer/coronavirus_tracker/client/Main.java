950711c94535662e2fa5b90f63efbd02559eb694
Mike Nußbaumer <info@mike-nussbaumer.com>
2020-06-14 15:31:59 +0200

    implemented first design idea

--- client/src/main/java/com/github/mikenussbaumer/coronavirus_tracker/client/Main.java
+++ client/src/main/java/com/github/mikenussbaumer/coronavirus_tracker/client/Main.java
@@ -4,19 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start ( Stage primaryStage ) throws Exception {
+        Parent root = FXMLLoader.load( getClass( ).getResource( "/main.fxml" ) );
+        primaryStage.setTitle( "Coronavirus Tracker" );
+
+        primaryStage.getIcons( ).add( new Image( Main.class.getResourceAsStream( "/assets/images/favicon.png" ) ) );
+        primaryStage.setScene( new Scene( root ) );
+        primaryStage.setResizable( false );
+        primaryStage.show( );
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main ( String[] args ) {
+        launch( args );
     }
 }
